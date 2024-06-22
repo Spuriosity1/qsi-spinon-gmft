d9f68087b2f989065694234dfe0b146462f7f2c8
Spuriosity1 <alaric.sanders@gmail.com>
2024-06-22 22:18:17 +0100

    started using Distributed (maybe this is less borked on HPC)

--- src/SimFunctions.jl
+++ src/SimFunctions.jl
@@ -1,134 +1,36 @@
-using LinearAlgebra
-using StaticArrays
+include("./IOFunctions.jl")
+
 using ProgressMeter
-using JLD
-using HDF5
-using Printf
 
-
-include("BZMath.jl")
-include("SpinonStructure.jl")
-
-using .BZmath
-using .SpinonStructure
-
-
+function calc_spinons_along_path(output_dir;
+    sim::SimulationParameters,
+    λ::Float64,
+    path::BZPath)
+    num_K = length(path.K)
+    bands = zeros(Float64, num_K, length(sim.lat.tetra_sites))
     
-function save_SQW(output_dir::String;
-        Spm::Union{Matrix{ComplexF64},Vector{ComplexF64}},
-        Spp::Union{Matrix{ComplexF64},Vector{ComplexF64}},
-        Smagnetic::Union{Matrix{Float64},Vector{Float64}},
-        bounds::Union{Nothing,Matrix{Float64}},
-        Egrid::Vector{Float64},
-        BZ_path::Union{Nothing,BZPath},
-        sim::SimulationParameters, 
-        ip::IntegrationParameters,
-        prefix="SQW")
-
-    name = output_dir*"/"*prefix*sim_identifier(sim)*".jld"
-    rm(name, force=true)
-    jldopen(name, "w") do file
-        g1 = create_group(file, "integration_parameters")
-        g1["n_K_samples"] = ip.n_K_samples
-        g1["broadening_dE"] = ip.broadening_dE
-        g1["version"] = 1.1
-#        g1["BZ_grid_density"] = ip.BZ_grid_density
-
-        g = create_group(file, "physical_parameters")
-        g["name"] = sim.name
-        g["emergent_fluxes"] = calc_fluxes(sim.lat, sim.A)
-        g["gauge"] = sim.A
-        g["Jpm"] = sim.Jpm
-        g["B"] = sim.B
-        g["L"]=sim.lat.L
-
-        d = create_group(file, "intensity") 
-        d["Spm"] = Spm
-        d["Spp"] = Spp
-        d["Smagnetic"] = Smagnetic
-        if bounds != nothing
-            d["bounds"] = bounds
+    prog=Progress(num_K, desc="Spinon Dispersion: ") 
+    @Threads.threads for J=1:num_K
+        bands[J,:] = begin
+            try
+                spinon_dispersion(path.K[J], sim, λ)[1]'
+            catch e
+                if e isa DomainError
+                    NaN*ones(Float64, 1,length(sim.lat.tetra_sites))
+                else
+                    throw(e)
+                end
+            end
         end
-        # a list of K points, such that the I'th S slics corresponds to the I'th K point
-        if BZ_path != nothing
-            d["Q_list"] = BZ_path.K 
-            d["tau"] = BZ_path.t
-            d["ticks_tau"] = BZ_path.ticks_t
-            d["ticks_label"] = BZ_path.ticks_label
-        end
-        d["W"] = Egrid
+        next!(prog)
     end
-    return name
+    finish!(prog)
+
+    return save_spinons(output_dir;
+        bands=bands,
+        BZ_path=path,
+        sim=sim)
 end
-
-
-function save_field_sweep(output_dir::String;
-        Spm::Union{Matrix{ComplexF64},Vector{ComplexF64}},
-        Spp::Union{Matrix{ComplexF64},Vector{ComplexF64}},
-        Smagnetic::Union{Matrix{Float64},Vector{Float64}},
-        Egrid::Vector{Float64},
-        magnetic_field_strengths::Vector{Float64},
-        sim::SimulationParameters, 
-        ip::IntegrationParameters,
-        prefix="int_fieldsweep")
-
-    name = output_dir*"/"*prefix*sim_identifier(sim)*".jld"
-    rm(name, force=true)
-    jldopen(name, "w") do file
-        g1 = create_group(file, "integration_parameters")
-        g1["n_K_samples"] = ip.n_K_samples
-        g1["broadening_dE"] = ip.broadening_dE
-        g1["version"] = 1.0
-
-        g = create_group(file, "physical_parameters")
-        g["name"] = sim.name
-        g["emergent_fluxes"] = calc_fluxes(sim.lat, sim.A)
-        g["gauge"] = sim.A
-        g["Jpm"] = sim.Jpm
-        g["B"] = sim.B
-        g["L"]=sim.lat.L
-
-        d = create_group(file, "integrated_intensity") 
-        d["Spm"] = Spm
-        d["Spp"] = Spp
-        d["Smagnetic"] = Smagnetic
-        d["W"] = Egrid
-        d["magnetic_field_strengths"] = magnetic_field_strengths
-    end
-    return name
-end
-
-
-
-function save_spinons(output_dir::String;
-        bands::Matrix{Float64},
-        BZ_path::BZPath,
-        sim::SimulationParameters, 
-        prefix="spinons")
-
-    name = output_dir*"/"*prefix*sim_identifier(sim)*".jld"
-    jldopen(name, "w") do file
-
-        g = create_group(file, "physical_parameters")
-        g["name"] = sim.name
-        g["fluxes"] = sim.A
-        g["Jpm"] = sim.Jpm
-        g["B"] = sim.B
-        g["L"]=sim.lat.L
-
-        d = create_group(file, "spinon_dispersion") 
-        d["bands"] = bands
-        # a list of K points, such that the I'th S slics corresponds to the I'th K point
-        d["Q_list"] = BZ_path.K 
-        d["tau"] = BZ_path.t
-        d["ticks_tau"] = BZ_path.ticks_t
-        d["ticks_label"] = BZ_path.ticks_label
-    end
-    return name
-end
-
-
-
 
 ###########
 # expensive calculations
@@ -200,7 +102,15 @@ function calc_spectral_weight_along_path(
 end
 
 
+"""
+function calc_integrated_specweight(
+    output_dir::String;
+    sim::SimulationParameters, λ::Float64,
+    ip::IntegrationParameters, Egrid::Vector{Float64},g_tensor=nothing)
 
+Calculates the BZ integrated spectral weight for a particular sim, 
+    saving results to output_dir
+"""
 function calc_integrated_specweight(
     output_dir::String;
     sim::SimulationParameters, λ::Float64,
@@ -348,37 +258,6 @@ function integrated_fieldsweep(output_dir::String;
 end
 
 
-
-
-function calc_spinons_along_path(output_dir;
-    sim::SimulationParameters,
-    λ::Float64,
-    path::BZPath)
-    num_K = length(path.K)
-    bands = zeros(Float64, num_K, length(sim.lat.tetra_sites))
-    
-    prog=Progress(num_K, desc="Spinon Dispersion: ") 
-    @Threads.threads for J=1:num_K
-        bands[J,:] = begin
-            try
-                spinon_dispersion(path.K[J], sim, λ)[1]'
-            catch e
-                if e isa DomainError
-                    NaN*ones(Float64, 1,length(sim.lat.tetra_sites))
-                else
-                    throw(e)
-                end
-            end
-        end
-        next!(prog)
-    end
-    finish!(prog)
-
-    return save_spinons(output_dir;
-        bands=bands,
-        BZ_path=path,
-        sim=sim)
-end
 
 
 # some useful defaults
