a22f129ee2420a775edb1c800163695a8be0dd37
Spuriosity1 <alaric.sanders@gmail.com>
2024-06-26 17:16:29 +0100

    another preset

--- src/SimFunctions.jl
+++ src/SimFunctions.jl
@@ -386,7 +386,7 @@ const integration_settings = Dict(
     "very_fast" => IntegrationParameters(n_K_samples=10,   broadening_dE=0.1),
     "fast" =>   IntegrationParameters(n_K_samples=100,  broadening_dE=0.05),
     "slow" =>      IntegrationParameters(n_K_samples=1000, broadening_dE=0.02),
-    "very_slow" => IntegrationParameters(n_K_samples=10000,broadening_dE=0.02)
+    "very_slow" => IntegrationParameters(n_K_samples=10000,broadening_dE=0.02),
     "ultra_slow" => IntegrationParameters(n_K_samples=100000,broadening_dE=0.02)
 )
 
